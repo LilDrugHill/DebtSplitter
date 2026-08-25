25ab1ed0477cb820c108ab3321f4d4c750f4a7f2
LilDrugHill <smookingrow@icloud.com>
2026-08-25 15:00:10 +0300

    feature(domain): add Entity package
    feature(domain.entities): add User entity
    
    fix(domain.entity): fix using setters in constructor
    
    feature(domain.entities): add Group entity
    
    fix(domain.exeptions): fix extensions accordingly guide
    
    feat(domain.entities): add Expense entity and improve validation
    
    feat(domain.entities): add Expense entity and improve validation
    
    refactor(domain.entities): refactor equals and constructor
    
    feat(domain): add testing init for entities and VablueO
    
    refactor(domain.entities): revise logic for dept output
    
    refactor(domain.entities): add validation
    
    fix(domain.entities): fix list process accordingly guide
    
    fix(domain.entities): fix email validation
    
    fix(domain): fix unchecked cast

--- src/main/java/com/debtsplitter/domain/exceptions/NonPositiveAmountException.java
+++ src/main/java/com/debtsplitter/domain/exceptions/NonPositiveAmountException.java
@@ -1,7 +1,18 @@
 package com.debtsplitter.domain.exceptions;
 
 public class NonPositiveAmountException extends DomainException {
-    public NonPositiveAmountException(String message) {
-        super(message);
+    private final String actual;
+
+    public NonPositiveAmountException(final String actual) {
+        super(formatMessage(actual));
+        this.actual = actual;
+    }
+    private static String formatMessage(String actual) {
+        return String.format("Non negative amound expected. Gotten: %s", actual);
+    }
+
+
+    public String getActual() {
+        return actual;
     }
 }
