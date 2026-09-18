06f8869d7af64c6de9aa108ed23ffdb31700b0c6
LilDrugHill <smookingrow@icloud.com>
2026-09-18 13:03:07 +0300

    refactor(model): moved

--- src/main/java/com/debtsplitter/domain/exceptions/DomainException.java
+++
@@ -1,7 +0,0 @@
-package com.debtsplitter.domain.exceptions;
-
-public abstract class DomainException extends RuntimeException {
-    public DomainException(String message) {
-        super(message);
-    }
-}
