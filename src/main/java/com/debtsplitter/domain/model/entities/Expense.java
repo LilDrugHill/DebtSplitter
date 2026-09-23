07e1963d1ff8f441475001df9414a9f504db7dcc
lildrughill <smookingrow@icloud.com>
2026-09-24 00:23:17 +0300

    refactor(domain): refactor accordingly new requirements

--- src/main/java/com/debtsplitter/domain/model/entities/Expense.java
+++ src/main/java/com/debtsplitter/domain/model/entities/Expense.java
@@ -2,23 +2,35 @@ package com.debtsplitter.domain.model.entities;
 
 import com.debtsplitter.domain.model.exceptions.NonPositiveAmountException;
 import com.debtsplitter.domain.model.exceptions.SelfDebtException;
-import com.debtsplitter.domain.model.valueObjects.ExpenseId;
-import com.debtsplitter.domain.model.valueObjects.GroupId;
-import com.debtsplitter.domain.model.valueObjects.Money;
-import com.debtsplitter.domain.model.valueObjects.UserId;
+import com.debtsplitter.domain.model.valueObjects.*;
+import com.debtsplitter.domain.split.SplitType;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
 import java.util.*;
 
 public class Expense {
     final private ExpenseId id;
     final private UserId paidBy;
     final private Money amount;
-    final private GroupId group;
-    final private Date createdAt = new Date();
-    final private Date spendAt;
+    final private GroupId groupId;
+
+    private String description;
+    private SplitType splitType;
+    private List<Share> shares;
+
+    private Instant createdAt;
+    private Instant spendAt;
 
 
-    public Expense(ExpenseId id, UserId paidBy, Money amount, GroupId group) {}
+    public Expense(UserId paidBy, Money amount, GroupId group, Instant spendAt) {
+        this(paidBy, amount, group);
+
+        if (spendAt != null) {
+            this.spendAt = spendAt;
+        }
+    }
 
     public Expense(UserId paidBy, Money amount, GroupId groupId) {
 
@@ -44,32 +56,28 @@ public class Expense {
         if (groupId == null) {
             throw  new IllegalArgumentException("group is null");
         }
-        if (group.getParticipants().contains(payer)) {
-            throw new SelfDebtException("%s@%s%d cant be owe to self".formatted(payer.getName(),
-                                                                                payer.getClass().getName(),
-                                                                                group.getId()));
-        }
-        this.group = group;
 
-        if (spendAt == null) {
-            ;
-        }
+        this.groupId = groupId;
 
+
+        var clock = Clock.systemDefaultZone();
+        createdAt = clock.instant();
+        spendAt = Clock.fixed(createdAt, clock.getZone()).instant();
     }
 
-    public HashMap<User, Money> getParticipantsDept() {
-        var guys = this.getGroup().getParticipants();
-        var gCount = guys.size();
-        List<Money> gAmounds = this.getAmount().divideOnParts(gCount);
-
-        var debtsDict = new HashMap<User, Money>();
-
-        for (int i = 0; i < gCount; i++) {
-            debtsDict.put(guys.get(i), gAmounds.get(i));
-        }
-
-        return  debtsDict;
-    }
+//    public HashMap<User, Money> getParticipantsDept() {
+//        var guys = this.getGroup().getParticipants();
+//        var gCount = guys.size();
+//        List<Money> gAmounds = this.getAmount().divideOnParts(gCount);
+//
+//        var debtsDict = new HashMap<User, Money>();
+//
+//        for (int i = 0; i < gCount; i++) {
+//            debtsDict.put(guys.get(i), gAmounds.get(i));
+//        }
+//
+//        return  debtsDict;
+//    }
 
     public ExpenseId getId() {
         return id;
@@ -83,9 +91,12 @@ public class Expense {
         return amount;
     }
 
-    public Group getGroup() {
-        return group;
+    public GroupId getGroup() {
+        return groupId;
     }
+
+    public Instant getCreatedAt() { return Clock.fixed(createdAt, ZoneId.systemDefault()).instant(); }
+    public Instant getSpendAt() { return Clock.fixed(spendAt, ZoneId.systemDefault()).instant(); }
 
     @Override
     public boolean equals(Object obj) {
