d81cdc13287fe866779b151db0674f1dab67bde0
lildrughill <smookingrow@icloud.com>
2026-09-24 00:27:49 +0300

    feat(domain): add balance calc

---
+++ src/main/java/com/debtsplitter/domain/service/BalanceCalculator.java
@@ -0,0 +1,14 @@
+package com.debtsplitter.domain.service;
+
+import com.debtsplitter.domain.model.entities.Expense;
+import com.debtsplitter.domain.model.valueObjects.Money;
+import com.debtsplitter.domain.model.valueObjects.UserId;
+
+import java.util.List;
+import java.util.Map;
+
+public class BalanceCalculator {
+    public static Map<UserId, Money> calculate(List<Expense> expenses) {
+
+    }
+}
